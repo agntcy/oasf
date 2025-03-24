fa54680cd4d39c55c5f28c93434ed383b9cca826
Luca Muscariello <lumuscar@cisco.com>
2025-03-24 08:52:07 +0100

    docs: copyright
    Signed-off-by: Luca Muscariello <lumuscar@cisco.com>

--- server/priv/static/js/app.js
+++ server/priv/static/js/app.js
@@ -1,13 +1,3 @@
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//   http://www.apache.org/licenses/LICENSE-2.0
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 function reset_home_page() {
   set_selected_extensions({});
   set_selected_profiles([]);
@@ -112,7 +102,7 @@ function display_attributes(options) {
     const length = rows.length;
 
     for (let i = 1; i < length; i++) {
-      const classList = rows[i].classList;      
+      const classList = rows[i].classList;
       const delta = intersection(array_to_set(classList), options);
       display_row(delta, classList);
     }
