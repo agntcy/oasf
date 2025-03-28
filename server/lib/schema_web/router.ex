9f60746143be7ad52eef6ee85d4b7ab9b87a391a
András Jáky <ajaky@cisco.com>
2025-03-28 14:09:06 +0100

    chore: add link to SwaggerUI back
    Signed-off-by: András Jáky <ajaky@cisco.com>

--- server/lib/schema_web/router.ex
+++ server/lib/schema_web/router.ex
@@ -191,11 +191,11 @@ defmodule SchemaWeb.Router do
     get "/features/:extension/:id", SchemaController, :sample_class
   end
 
-  # scope "/doc" do
-  #  forward "/", PhoenixSwagger.Plug.SwaggerUI,
-  #    otp_app: :schema_server,
-  #    swagger_file: "swagger.json"
-  # end
+  scope "/doc" do
+    forward "/", PhoenixSwagger.Plug.SwaggerUI,
+      otp_app: :schema_server,
+      swagger_file: "swagger.json"
+  end
 
   def swagger_info do
     %{
