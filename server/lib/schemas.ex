c15074eab306fba7b083d8385ba38c060fef8b42
András Jáky <ajaky@cisco.com>
2025-08-04 13:53:52 +0200

    feat(server): update server with upstream changes (#231)
    * fix: patching with inheritance
    
    as per
    https://github.com/ocsf/ocsf-server/commit/f1648f1ec8bba5ce0e6d1b19c2fd6c19446ef515
    
    Signed-off-by: András Jáky <ajaky@cisco.com>
    
    * chore: use Utils.descope instead of Path.basename
    
    Signed-off-by: András Jáky <ajaky@cisco.com>
    
    * fix: enum siblings, attribute validation issues
    
    Fix patch bug where attribute descriptions were incorrectly copied back
    to the dictionary as per
    https://github.com/ocsf/ocsf-server/commit/39a8269a727de15b8d670fa26170fe015cf1bf7d
    
    Signed-off-by: András Jáky <ajaky@cisco.com>
    
    * feat: sort attributes by descoped key
    
    Signed-off-by: András Jáky <ajaky@cisco.com>
    
    * fix: presentation of enum siblings
    
    by deferring calculation to presentation time (HTTP request handling).
    This avoids "bleed" of the internal `_sibling_of` attribute to cases
    where an attribute is not used as an enum sibling.
    
    https://github.com/ocsf/ocsf-server/commit/0f63278bae36f10dc3db5097f1a188a20ecb4e77
    Signed-off-by: András Jáky <ajaky@cisco.com>
    
    * chore: use error function at all places
    
    Signed-off-by: András Jáky <ajaky@cisco.com>
    
    * feat: add OASF schema_version validation
    
    as per
    https://github.com/ocsf/ocsf-server/commit/27532f34b706faef59a1e1fe6c1df65bc10db29d
    
    Signed-off-by: András Jáky <ajaky@cisco.com>
    
    * test: add schema compilation test
    
    as per
    https://github.com/ocsf/ocsf-server/commit/916826a2a27cdd079bdbf0f7ed0e91efd61c5200
    and
    https://github.com/ocsf/ocsf-server/commit/e4087683cc1bc4b715984725dda0492109b6f5e1
    
    Signed-off-by: András Jáky <ajaky@cisco.com>
    
    * fix: datetime profile construction in extended objects
    
    as per
    https://github.com/ocsf/ocsf-server/commit/e838beac9f0738526c6459acf2806cfd3abdf0b8
    
    Signed-off-by: András Jáky <ajaky@cisco.com>
    
    * feat: class attribute associations when patching
    
    as per
    https://github.com/ocsf/ocsf-server/commit/7a1422bc279d2d829abc1d9bc333b5d13f0c7aaf
    
    Signed-off-by: András Jáky <ajaky@cisco.com>
    
    ---------
    
    Signed-off-by: András Jáky <ajaky@cisco.com>

--- server/lib/schemas.ex
+++ server/lib/schemas.ex
@@ -16,9 +16,8 @@ defmodule Schemas do
 
   def start_link(schema_versions) do
     # Split the string by commas into a list
-    parsed_versions = String.split(schema_versions, ",")
-    IO.inspect(parsed_versions, label: "Parsed schema versions")
-    Agent.start_link(fn -> parse_versions(parsed_versions) end, name: __MODULE__)
+    versions = String.split(schema_versions, ",")
+    Agent.start_link(fn -> parse_versions(versions) end, name: __MODULE__)
   end
 
   @doc """
@@ -34,15 +33,30 @@ defmodule Schemas do
   Expects a list of versions or a map with a "versions" key.
   Returns a list of {version, metadata} tuples in case of success, or an empty list if the content is invalid.
   """
-  @spec parse_versions(any()) :: list({String.t(), map()})
   def parse_versions(%{"versions" => versions}) when is_list(versions) do
-    # Transform each version into a tuple {version, metadata}
-    Enum.map(versions, fn version -> {version, %{}} end)
+    versions
+    |> Enum.map(fn version ->
+      case Schema.Utils.parse_version(version) do
+        parsed when is_map(parsed) -> {version, parsed}
+        _error -> nil
+      end
+    end)
+    |> Enum.filter(& &1)
+    |> Enum.sort(fn {_, v1}, {_, v2} -> Schema.Utils.version_sorter(v1, v2) end)
+    |> Enum.map(fn {version, _parsed} -> {version, %{}} end)
   end
 
   def parse_versions(versions) when is_list(versions) do
-    # Assume the input is already a list of version strings
-    Enum.map(versions, fn version -> {version, %{}} end)
+    versions
+    |> Enum.map(fn version ->
+      case Schema.Utils.parse_version(version) do
+        parsed when is_map(parsed) -> {version, parsed}
+        _error -> nil
+      end
+    end)
+    |> Enum.filter(& &1)
+    |> Enum.sort(fn {_, v1}, {_, v2} -> Schema.Utils.version_sorter(v1, v2) end)
+    |> Enum.map(fn {version, _parsed} -> {version, %{}} end)
   end
 
   def parse_versions(_invalid_data) do
